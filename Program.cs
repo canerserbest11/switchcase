bbfabfeac94a647ce6c765053c5ee500d8b76c69
caner <canerserbest1997@gmail.com>
2022-01-17 22:32:24 +0300

    switch

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mont = DateTime.Now.Month;
+
+            switch (mont)
+            {
+                case 1:
+                Console.WriteLine("Ocak");
+                break;
+                case 2:
+                Console.WriteLine("Şubat");
+                break;
+                case 3:
+                Console.WriteLine("Mart");
+                break;
+                case 4:
+                Console.WriteLine("Nisan");
+                break;
+                case 5:
+                Console.WriteLine("Mayıs");
+                break;
+                case 6:
+                Console.WriteLine("Haziran");
+                break;
+                case 7:
+                Console.WriteLine("Temmuz");
+                break;
+                case 8:
+                Console.WriteLine("Ağustos");
+                break;
+                case 9:
+                Console.WriteLine("Eylül");
+                break;
+                case 10:
+                Console.WriteLine("Ekim");
+                break;
+                case 11:
+                Console.WriteLine("Kasım");
+                break;
+                case 12:
+                Console.WriteLine("Aralık");
+                break;
+                default:
+                break;
+            }
+            switch (mont)
+            {
+                case 12:
+                case 1:
+                case 2:
+                     Console.WriteLine("Kış");
+                     break;
+                case 3:
+                case 4:
+                case 5:
+                     Console.WriteLine("İlkbahar");
+                     break;
+                case 6:
+                case 7:
+                case 8:
+                     Console.WriteLine("Yaz");
+                     break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                default:
+                break;
+            }
+        }
+    }
+}
